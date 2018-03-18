5d6f652d4e9fb9b630af44e8f04138954c365662
SunGuoQiang123 <993190138@qq.com>
2018-03-18 12:55:09 +0800

    some changes about code

--- js/background.js
+++ js/background.js
@@ -1,29 +1,49 @@
-chrome.contextMenus.create({
-    title: '使用度娘搜索: %s',
-    contexts: ['selection'],
-    onclick: function(params) {
-        chrome.tabs.create({url:'https://www.baidu.com/s?ie=UTF-8&wd=' + encodeURI(params.selectionText)});
-    }
-})
-
+const cateList = ['movie', 'code'];
+const cateItems = {
+    movie: ['猫眼搜索', '豆瓣搜索'],
+    code: ['github搜索', 'stackoverflow搜索']
+}
 
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
     console.log('input change is ', text);
+    let searchInfo = getSearchInfo(text);
+    searchInfo && showSuggestionList(searchInfo, suggest)
+    // if (text.startsWith('movie')){
+    //     suggest([
+    //         // {content:'毒蛇', description: '打开毒蛇或用毒蛇搜索'},
+    //         {content:'豆瓣搜索'+text.slice(6), description: '豆瓣搜索'+text.slice(6)},
+    //         {content:'猫眼搜索'+text.slice(6), description: '猫眼搜索'+text.slice(6)}
+    //     ])
+    // } else if(text.startsWith('code')) {
+    //     suggest([
+    //         {content:'github搜索' + text.slice(5), description: 'github搜索' + text.slice(5)}
+    //     ])
+    // }
+})
+
+function getServerCate(text) {
     if (!text) {
         return;
     }
-    if (text.startsWith('movie')){
-        suggest([
-            // {content:'毒蛇', description: '打开毒蛇或用毒蛇搜索'},
-            {content:'豆瓣搜索'+text.slice(6), description: '豆瓣搜索'+text.slice(6)},
-            {content:'猫眼搜索'+text.slice(6), description: '猫眼搜索'+text.slice(6)}
-        ])
-    } else if(text.startsWith('code')) {
-        suggest([
-            {content:'github搜索' + text.slice(5), description: 'github搜索' + text.slice(5)}
-        ])
+    text = text.toLowerCase();
+    let cate = cateList.find(cate => text.startsWith(cate));
+    let keyword = text.slice(cate.length);
+    return cate ? {cate, keyword} : void 0;
+}
+
+function showSuggestionList(searchInfo, suggest) {
+    for (key in cateItems) {
+        if (cateItems.hasOwnProperty(key)) {
+            if (key === searchInfo.cate) {
+                suggest(cateItems[key].map(item => {
+                    let displayContent = item + searchInfo.keyword;
+                    return {content: displayContent, description: displayContent}
+                }))
+            }
+        }
     }
-})
+}
+
 chrome.omnibox.onInputEntered.addListener((text) => {
     console.log('input entered is ', text);
     var href = '';
@@ -36,6 +56,9 @@ chrome.omnibox.onInputEntered.addListener((text) => {
     } else if (text.startsWith('github')) {
         text = text.slice(8);
         href = "https://github.com/search?utf8=%E2%9C%93&q=" + text;
+    } else if (text.startsWith('stackoverflow')) {
+        text = text.slice(15);
+        href = 'https://stackoverflow.com/search?q=' + text;
     }
     openUrlCurrentTab(href)
 })
@@ -45,6 +68,7 @@ function getCurrentTabId(callback) {
         if (callback) callback(tabs.length ? tabs[0].id : null)
     })
 }
+
 function openUrlCurrentTab(url) {
     getCurrentTabId((tabId) => {
         chrome.tabs.update(tabId, {url:url})
